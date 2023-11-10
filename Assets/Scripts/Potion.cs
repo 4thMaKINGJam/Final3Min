a1bb0be6a0041a4bc3de9f4f9f1c14016bd5d6c8
Ssomewhat <isc10120@gmail.com>
2023-11-10 22:19:47 +0900

    게임매니저, 포션, 폴더구조

---
+++ Assets/Scripts/Potion.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Potion : MonoBehaviour
+{
+    public int Base;
+    public int Item1;
+    public int Item2;
+    public int Cooked;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
