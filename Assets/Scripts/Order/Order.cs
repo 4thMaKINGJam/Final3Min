c6fef8062c87d58e07588866d9dc657df8cfbc3a
jiyoung <jiyoung_06@ewhain.net>
2023-11-11 15:02:06 +0900

    [Add] potion sprites

--- Assets/Scripts/Order/Order.cs
+++ Assets/Scripts/Order/Order.cs
@@ -9,13 +9,24 @@ public class Order : MonoBehaviour
     Stopwatch stopwatch;
     //private Potion potion;
     [SerializeField] private Slider _slider;
+    [SerializeField] private Sprite[] BaseImg;
+    [SerializeField] private Sprite[] ItemImg;
 
     private float ORDER_TIME = 24f * 1000;
     public bool timeOver;
     public int price;
+
+    private int ITEM_COUNT = 4;
+
     private Color GREEN = Color.green;
     private Color YELLOW = Color.yellow;
     private Color RED = Color.red;
+
+    private void Awake()
+    {
+        PickBase();
+
+    }
 
 
     // Start is called before the first frame update
@@ -27,6 +38,41 @@ public class Order : MonoBehaviour
         SetValue(0);
         //92D050
         _slider.transform.GetChild(0).GetComponent<Image>().color = GREEN;
+    }
+
+    void PickBase()
+    {
+
+    }
+
+    void PickItems()
+    {
+        System.Random prng = new System.Random();
+        
+        List<int> potion_item = new List<int>();
+
+        // number of item
+        int itemCount = prng.Next(1, 3);
+
+        // assign item
+        for (int i = 0; i < itemCount; i++)
+        {
+            potion_item.Add(1);
+        }
+        for (int i = 0; i < ITEM_COUNT - itemCount; i++)
+        {
+            potion_item.Add(0);
+        }
+
+        // shuffle
+        for (int i=0; i < ITEM_COUNT; i++)
+        {
+            int random_idx = prng.Next(i, ITEM_COUNT - 1);
+
+            int temp = potion_item[random_idx];
+            potion_item[random_idx] = potion_item[i];
+            potion_item[i] = temp;
+        }
     }
 
     // Update is called once per frame
