40b766a961c6666fb8b325d079c22fc7b16e90e5
dubidubob <sildredel@naver.com>
2023-11-12 04:10:29 +0900

    coder's night...

--- Assets/Scripts/Beaker/beaker1.cs
+++ Assets/Scripts/Beaker/beaker1.cs
@@ -1,19 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class beaker : MonoBehaviour
-{
-    [HideInInspector]
+    /*[HideInInspector]
     public int food = 0;
-    /*포션에게 전달해야할 것*/
+
     public int liquid { get; private set; }
     public int[] item { get; private set; } = new int[4];
     public int cooked { get; private set; }
-    public int itemCnt { get; private set; }
-    [SerializeField] private Sprite[] BeakerImage;
-
-    int foodCnt;
 
     float waitTime;
     private Coroutine beakerTimer;
@@ -22,10 +12,8 @@ public class beaker : MonoBehaviour
     private GameObject fire;
     private Animator animator;
     
-    
 
-    void OnEnable() {
-        Debug.Log("B. 초기화 됐습니다!");
+    void OnEnable() {//enable 
         food = 7;
         liquid = 0;
         for (int i = 0; i < item.Length; i++)
@@ -33,57 +21,34 @@ public class beaker : MonoBehaviour
             item[i] = 0;
         }
         cooked = 0;
-        itemCnt = 0;
-        foodCnt = 0;
         waitTime = 7.0f;
         
         animator = this.GetComponent<Animator>();
-        animator.SetInteger("color", 0);
+        animator.enabled = false;
         animator.SetInteger("cooked", cooked);
-        this.GetComponent<SpriteRenderer>().sprite = BeakerImage[0];
-
-
-
     }
 
-        void Update()
+    void Update()
     {
-        if (food !=7)
+        if (food != 7)
         {
             checkFood(food);
             food = 7;
+            foodCnt++;
+            timerCheck(foodCnt);
         }
     }
-
-    #region check food
-    void checkFood(int foodType)
-    {
-        if (foodType < 4)//food가 재료면 item에 들어왔다고 추가
-        {
-            Debug.Log("B. food 들어왔어요:"+ foodType);
-            itemCnt++;
-            item[foodType]++;
-        }
-        else if (foodType < 7)// food가 액체면 liquid에 추가, 비커 색깔 바꾸기
-        {
-            liquid = foodType;
-            changeColor(liquid);
-        }
-        foodCnt++;
-        timerCheck(foodCnt);
-        return;
-    }
-    #endregion
 
     #region change color
     void changeColor(int baseColor) {
+        animator.enabled = true;
         animator.SetInteger("color", baseColor);
     }
     #endregion
 
     #region beaker timer
     /***아이템이 들어올 때 7초, 3초 추가***/
-    void timerCheck(int count)
+    /*void timerCheck(int count)
     {
         Debug.Log("시간 잴게요~" + count);
         if (count == 1)//처음이라면 7초 타이머 시작
@@ -110,15 +75,11 @@ public class beaker : MonoBehaviour
 
     IEnumerator TimerFinished()
     {
-        int timeExm = 3;
         cooked = 1;
-        Debug.Log("B. 완성됐습니다." + cooked);
         animator.SetInteger("cooked", cooked);//1
-        while (timeExm>0) {
-            yield return new WaitForSeconds(1f);
-            timeExm--;
-            Debug.Log("지금입니다. 성공의 시간!" + cooked);
-        }
+        yield return new WaitForSeconds(3f);
+        
+        fire.SetActive(false);
         cooked = 2;
         animator.SetInteger("cooked", cooked);
         Debug.Log("B. 끝났수"+cooked);
@@ -128,9 +89,7 @@ public class beaker : MonoBehaviour
     void AddExtraTime()
     {
         waitTime += 3;
-        //기존 코루틴 중지, 갱신된 시간으로 타이머 재시작
         StopCoroutine(beakerTimer);
-        Debug.Log("B. 3초 추가!");
         beakerTimer = StartCoroutine(StartTimer());
     }
 
@@ -139,12 +98,11 @@ public class beaker : MonoBehaviour
     }
     #endregion
 
-    public void initiate()//비커 설정 초기화
+    public void initiate()
     {
-        Debug.Log("비커 초기화");
         StopCouroutine();
         fire.SetActive(false);
-        animator.SetInteger("color", 0);//init으로 돌아옴
         this.enabled = false;
     }
 }
+*/
